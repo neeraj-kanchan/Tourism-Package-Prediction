c42ee9224e7b16064dc6c63a730e5b5c572659c6
neeraj-kanchan <neerajkanchan@gmail.com>
2026-08-23 14:54:34 +0530

    update tourism_project/model_building/prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -2,11 +2,9 @@ import os
 import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
-from huggingface_hub import login, HfApi
 
-api = HfApi(token=os.getenv("GH_TOKEN"))
-DATASET_LOC = "hf://datasets/neeraj-kanchan/Tourism-Package-Prediction/tourism.csv"
-bank_dataset = pd.read_csv(DATASET_LOC)
+DATASET_LOC = "tourism_project/data/tourism.csv"
+input_dataset = pd.read_csv(DATASET_LOC)
 print("Dataset loaded successfully.")
 
 # Target variable
@@ -39,10 +37,10 @@ categorical_features = [
 ]
 
 # Predictor matrix (X)
-X = bank_dataset[numeric_features + categorical_features]
+X = input_dataset[numeric_features + categorical_features]
 
 # Target variable
-y = bank_dataset[target]
+y = input_dataset[target]
 
 # Spliting dataset into train and test
 Xtrain, Xtest, ytrain, ytest = train_test_split(
@@ -51,17 +49,10 @@ Xtrain, Xtest, ytrain, ytest = train_test_split(
     random_state=42    # Fixed random seed to be able to reproduce the results.
 )
 
-Xtrain.to_csv("Xtrain.csv",index=False)
-Xtest.to_csv("Xtest.csv",index=False)
-ytrain.to_csv("ytrain.csv",index=False)
-ytest.to_csv("ytest.csv",index=False)
+Xtrain.to_csv("tourism_project/data/Xtrain.csv",index=False)
+Xtest.to_csv("tourism_project/data/Xtest.csv",index=False)
+ytrain.to_csv("tourism_project/data/ytrain.csv",index=False)
+ytest.to_csv("tourism_project/data/ytest.csv",index=False)
 
 files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
 
-for file_path in files:
-    api.upload_file(
-        path_or_fileobj=file_path,
-        path_in_repo=file_path.split("/")[-1],  # just the filename
-        repo_id="neeraj-kanchan/Tourism-Package-Prediction",
-        repo_type="dataset",
-    )
